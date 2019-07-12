e20c63769a0c7c3cb77111f4d0558cbc0f58437a
Mitch Downey <mitchldowney@gmail.com>
2019-07-12 01:03:18 -0500

    Update runLatestPodverseApiWorkers.sh

--- scripts/runLatestPodverseApiWorkers.sh
+++ scripts/runLatestPodverseApiWorkers.sh
@@ -1,10 +1,4 @@
 docker rm $(docker stop $(docker ps -a -q --filter ancestor=podverse/podverse_api))
 docker image rm podverse/podverse_api
-docker-compose -f /home/mitch/podverse-ops/docker-compose.$1.yml run -d --name podverse_api_parser_worker_1h podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 1 3600000
-docker-compose -f /home/mitch/podverse-ops/docker-compose.$1.yml run -d --name podverse_api_parser_worker_1g podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 1 3600000
-docker-compose -f /home/mitch/podverse-ops/docker-compose.$1.yml run -d --name podverse_api_parser_worker_1f podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 1 3600000
-docker-compose -f /home/mitch/podverse-ops/docker-compose.$1.yml run -d --name podverse_api_parser_worker_1e podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 1 3600000
-docker-compose -f /home/mitch/podverse-ops/docker-compose.$1.yml run -d --name podverse_api_parser_worker_1d podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 1 3600000
-docker-compose -f /home/mitch/podverse-ops/docker-compose.$1.yml run -d --name podverse_api_parser_worker_1c podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 1 3600000
 docker-compose -f /home/mitch/podverse-ops/docker-compose.$1.yml run -d --name podverse_api_parser_worker_1b podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 1 3600000
 docker-compose -f /home/mitch/podverse-ops/docker-compose.$1.yml run -d --name podverse_api_parser_worker_1a podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 1 3600000
