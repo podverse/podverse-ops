187dcee780ed327ae100f02be75aefa53d322580
Mitch Downey <mitchldowney@gmail.com>
2019-11-24 22:47:07 -0600

    Create backup_repos.sh

---
+++ scripts/backup_repos.sh
@@ -0,0 +1,23 @@
+cd $1/podcast-db
+git pull origin master
+
+cd $1/podverse-admin
+git pull origin master
+
+cd $1/podverse-api
+git pull origin master
+
+cd $1/podverse-ops
+git pull origin master
+
+cd $1/podverse-qa
+git pull origin master
+
+cd $1/podverse-rn
+git pull origin master
+
+cd $1/podverse-ui
+git pull origin master
+
+cd $1/podverse-web
+git pull origin master
