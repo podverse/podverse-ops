0778e54825cf714b21cbb249233b5bfde87dc2bd
Mitch Downey <mitchldowney@gmail.com>
2019-11-24 22:24:39 -0600

    Create clone_all_repos.sh

---
+++ scripts/clone_all_repos.sh
@@ -0,0 +1,8 @@
+/usr/bin/git clone https://github.com/podverse/podcast-db.git
+/usr/bin/git clone https://github.com/podverse/podverse-admin.git
+/usr/bin/git clone https://github.com/podverse/podverse-api.git
+/usr/bin/git clone https://github.com/podverse/podverse-ops.git
+/usr/bin/git clone https://github.com/podverse/podverse-qa.git
+/usr/bin/git clone https://github.com/podverse/podverse-rn.git
+/usr/bin/git clone https://github.com/podverse/podverse-ui.git
+/usr/bin/git clone https://github.com/podverse/podverse-web.git
