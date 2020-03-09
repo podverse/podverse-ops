7dcc78c269ef772fb1cd70321da892b71b3683a2
Mitch Downey <mitchldowney@gmail.com>
2020-03-09 00:07:27 -0500

    Create restore-stage-database.sh

---
+++ scripts/jenkins/restore-stage-database.sh
@@ -0,0 +1,5 @@
+docker stop podverse_db_stage;
+docker rm podverse_db_stage;
+docker-compose -f ./podverse-ops/docker-compose.stage.yml up -d podverse_db;
+sleep 10;
+PGPASSWORD='<password here>' psql -h localhost -U postgres -d postgres -f qa-database.sql;
