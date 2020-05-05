0f93f7ee9b71af497ba6448d855bd17fcd4ec2fe
Mitch Downey <mitchldowney@gmail.com>
2020-05-04 23:35:14 -0500

    Update runLatestPodverseWeb.sh

--- scripts/runLatestPodverseWeb.sh
+++ scripts/runLatestPodverseWeb.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 docker rm $(docker stop $(docker ps -a -q --filter ancestor=podverse/podverse_web))
 docker image rm podverse/podverse_web
 docker-compose -f /home/mitch/podverse-ops/docker-compose.$1.yml up -d podverse_web
