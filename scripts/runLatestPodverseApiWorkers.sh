cf4846cb87c1e22bac0b9688b2d474bc6579bfc2
Mitch Downey <mitchldowney@gmail.com>
2020-09-27 17:28:45 -0500

    Update runLatestPodverseApiWorkers.sh

--- scripts/runLatestPodverseApiWorkers.sh
+++ scripts/runLatestPodverseApiWorkers.sh
@@ -8,3 +8,5 @@ docker image rm podverse/podverse_api
 /usr/local/bin/docker-compose -f /home/mitch/podverse-ops/docker-compose.$1.yml run -d --name podverse_api_parser_worker_6 podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 0
 /usr/local/bin/docker-compose -f /home/mitch/podverse-ops/docker-compose.$1.yml run -d --name podverse_api_parser_worker_7 podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 0
 /usr/local/bin/docker-compose -f /home/mitch/podverse-ops/docker-compose.$1.yml run -d --name podverse_api_parser_worker_8 podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 0
+/usr/local/bin/docker-compose -f /home/mitch/podverse-ops/docker-compose.$1.yml run -d --name podverse_api_parser_worker_9 podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 0
+/usr/local/bin/docker-compose -f /home/mitch/podverse-ops/docker-compose.$1.yml run -d --name podverse_api_parser_worker_10 podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 0
