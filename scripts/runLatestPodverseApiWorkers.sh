b25a348b513c9d4a7d9f047b7e43386c9fe934dd
Mitch Downey <mitchldowney@gmail.com>
2020-11-17 23:57:55 -0600

    Update runLatestPodverseApiWorkers.sh

--- scripts/runLatestPodverseApiWorkers.sh
+++ scripts/runLatestPodverseApiWorkers.sh
@@ -6,3 +6,4 @@ docker image rm podverse/podverse_api
 /usr/local/bin/docker-compose -f /home/mitch/podverse-ops/docker-compose.$1.yml run -d --name podverse_api_parser_worker_priority_4 podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 150000 priority
 /usr/local/bin/docker-compose -f /home/mitch/podverse-ops/docker-compose.$1.yml run -d --name podverse_api_parser_worker_priority_5 podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 150000 priority
 /usr/local/bin/docker-compose -f /home/mitch/podverse-ops/docker-compose.$1.yml run -d --name podverse_api_parser_worker_priority_6 podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 150000 priority
+/usr/local/bin/docker-compose -f /home/mitch/podverse-ops/docker-compose.$1.yml run -d --name podverse_api_parser_worker_priority_7 podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 150000 priority
