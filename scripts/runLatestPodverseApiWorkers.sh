a5193d3bfb1cb421dc100401a305430a96fb7636
Mitch Downey <mitchldowney@gmail.com>
2020-11-18 09:36:08 -0600

    Update runLatestPodverseApiWorkers.sh

--- scripts/runLatestPodverseApiWorkers.sh
+++ scripts/runLatestPodverseApiWorkers.sh
@@ -4,6 +4,7 @@ docker image rm podverse/podverse_api
 /usr/local/bin/docker-compose -f /home/mitch/podverse-ops/docker-compose.$1.yml run -d --name podverse_api_parser_worker_priority_2 podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 150000 priority
 /usr/local/bin/docker-compose -f /home/mitch/podverse-ops/docker-compose.$1.yml run -d --name podverse_api_parser_worker_priority_3 podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 150000 priority
 /usr/local/bin/docker-compose -f /home/mitch/podverse-ops/docker-compose.$1.yml run -d --name podverse_api_parser_worker_priority_4 podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 150000 priority
-/usr/local/bin/docker-compose -f /home/mitch/podverse-ops/docker-compose.$1.yml run -d --name podverse_api_parser_worker_priority_5 podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 150000 priority
-/usr/local/bin/docker-compose -f /home/mitch/podverse-ops/docker-compose.$1.yml run -d --name podverse_api_parser_worker_priority_6 podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 150000 priority
-/usr/local/bin/docker-compose -f /home/mitch/podverse-ops/docker-compose.$1.yml run -d --name podverse_api_parser_worker_priority_7 podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 150000 priority
+/usr/local/bin/docker-compose -f /home/mitch/podverse-ops/docker-compose.$1.yml run -d --name podverse_api_parser_worker_5 podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 300000
+/usr/local/bin/docker-compose -f /home/mitch/podverse-ops/docker-compose.$1.yml run -d --name podverse_api_parser_worker_6 podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 300000
+/usr/local/bin/docker-compose -f /home/mitch/podverse-ops/docker-compose.$1.yml run -d --name podverse_api_parser_worker_7 podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 300000
+/usr/local/bin/docker-compose -f /home/mitch/podverse-ops/docker-compose.$1.yml run -d --name podverse_api_parser_worker_8 podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 300000
