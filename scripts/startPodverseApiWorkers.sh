5f184c7556c00280782a3bac82689d8faf7dc4e9
Mitch Downey <mitchldowney@gmail.com>
2020-11-18 09:35:40 -0600

    Update startPodverseApiWorkers.sh

--- scripts/startPodverseApiWorkers.sh
+++ scripts/startPodverseApiWorkers.sh
@@ -2,5 +2,7 @@
 /usr/bin/docker start podverse_api_parser_worker_priority_2
 /usr/bin/docker start podverse_api_parser_worker_priority_3
 /usr/bin/docker start podverse_api_parser_worker_priority_4
-/usr/bin/docker start podverse_api_parser_worker_priority_5
-/usr/bin/docker start podverse_api_parser_worker_priority_6
+/usr/bin/docker start podverse_api_parser_worker_5
+/usr/bin/docker start podverse_api_parser_worker_6
+/usr/bin/docker start podverse_api_parser_worker_7
+/usr/bin/docker start podverse_api_parser_worker_8
