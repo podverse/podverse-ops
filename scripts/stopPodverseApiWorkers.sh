00cea0ff4f0c599a393d037204c62928c21a01fe
Mitch Downey <mitchldowney@gmail.com>
2020-11-23 20:58:10 -0600

    Update stopPodverseApiWorkers.sh

--- scripts/stopPodverseApiWorkers.sh
+++ scripts/stopPodverseApiWorkers.sh
@@ -1,1 +1,8 @@
-docker stop $(docker ps -a -q --filter ancestor=podverse/podverse_api)
+/usr/bin/docker stop podverse_api_parser_worker_priority_1
+/usr/bin/docker stop podverse_api_parser_worker_priority_2
+/usr/bin/docker stop podverse_api_parser_worker_priority_3
+/usr/bin/docker stop podverse_api_parser_worker_priority_4
+/usr/bin/docker stop podverse_api_parser_worker_5
+/usr/bin/docker stop podverse_api_parser_worker_6
+/usr/bin/docker stop podverse_api_parser_worker_7
+/usr/bin/docker stop podverse_api_parser_worker_8
