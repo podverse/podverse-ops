7d55645b65bd5281c12d2d50ec19628f269f1f81
Mitch Downey <mitchldowney@gmail.com>
2022-03-04 23:41:05 -0600

    Update generate_episode_export_local.sh

--- manticore/generate_episode_export_local.sh
+++ manticore/generate_episode_export_local.sh
@@ -1,4 +1,4 @@
-psql -h 0.0.0.0 -p 5432 -U postgres -c "COPY episodes(id, title, "\"pastHourTotalUniquePageviews\"", "\"pastDayTotalUniquePageviews\"", "\"pastWeekTotalUniquePageviews\"", "\"pastMonthTotalUniquePageviews\"", "\"pastYearTotalUniquePageviews\"", "\"pastAllTimeTotalUniquePageviews\"") TO stdout DELIMITER ',' CSV;" > ./manticore/episode_export.csv;
+PGPASSWORD=mysecretpw psql -h 0.0.0.0 -p 5432 -U postgres -c "COPY episodes(id, title, "\"pastHourTotalUniquePageviews\"", "\"pastDayTotalUniquePageviews\"", "\"pastWeekTotalUniquePageviews\"", "\"pastMonthTotalUniquePageviews\"", "\"pastYearTotalUniquePageviews\"", "\"pastAllTimeTotalUniquePageviews\"") TO stdout DELIMITER ',' CSV;" > ./manticore/episode_export.csv;
 awk '{printf "%s,%s\n", NR,$0}' ./manticore/episode_export.csv > ./manticore/episode_export_with_serial_column.csv;
 rm ./manticore/episode_export.csv;
 docker exec -it podverse_manticore_local gosu manticore indexer --all --rotate;
