f13df707244a52d7fd6a385d382c62037bbd9084
Mitch Downey <mitchldowney@gmail.com>
2022-03-05 12:14:54 -0600

    Split Manticore episode csv into 5,000,000 rows instead of 10,000,000

--- manticore/generate_episode_export_prod.sh
+++ manticore/generate_episode_export_prod.sh
@@ -1,9 +1,9 @@
 PGPASSWORD='mysecretpw' psql -h 0.0.0.0 -p 5432 -U postgres -c "COPY episodes(id, title, "\"pastHourTotalUniquePageviews\"", "\"pastDayTotalUniquePageviews\"", "\"pastWeekTotalUniquePageviews\"", "\"pastMonthTotalUniquePageviews\"", "\"pastYearTotalUniquePageviews\"", "\"pastAllTimeTotalUniquePageviews\"") TO stdout DELIMITER ',' CSV;" > /home/mitch/podverse-ops/manticore/episode_export.csv;
 awk '{printf "%s,%s\n", NR,$0}' /home/mitch/podverse-ops/manticore/episode_export.csv > /home/mitch/podverse-ops/manticore/episode_export_with_serial_column.csv;
 rm /home/mitch/podverse-ops/manticore/episode_export.csv;
-split -l 10000000 /home/mitch/podverse-ops/manticore/episode_export_with_serial_column.csv /home/mitch/podverse-ops/manticore/episode_export_csvs/episode_export_ -d --additional-suffix=.csv
-docker exec -it podverse_manticore_prod gosu manticore idx_author --rotate;
-docker exec -it podverse_manticore_prod gosu manticore idx_media_ref --rotate;
-docker exec -it podverse_manticore_prod gosu manticore idx_playlist --rotate;
-docker exec -it podverse_manticore_prod gosu manticore idx_podcast --rotate;
-docker exec -it podverse_manticore_prod gosu manticore idx_episode idx_episode_01 idx_episode_02 idx_episode_03 idx_episode_04 idx_episode_05 idx_episode_06 idx_episode_07 idx_episode_08 idx_episode_09 idx_episode_10 idx_episode_11 idx_episode_12 idx_episode_13 idx_episode_14 idx_episode_15 idx_episode_16 idx_episode_17 idx_episode_18 idx_episode_19 --merge;
+split -l 5000000 /home/mitch/podverse-ops/manticore/episode_export_with_serial_column.csv /home/mitch/podverse-ops/manticore/episode_export_csvs/episode_export_ -d --additional-suffix=.csv
+docker exec -it podverse_manticore_prod gosu manticore indexer idx_author --rotate;
+docker exec -it podverse_manticore_prod gosu manticore indexer idx_media_ref --rotate;
+docker exec -it podverse_manticore_prod gosu manticore indexer idx_playlist --rotate;
+docker exec -it podverse_manticore_prod gosu manticore indexer idx_podcast --rotate;
+docker exec -it podverse_manticore_prod gosu manticore idx_episode idx_episode_01 idx_episode_02 idx_episode_03 idx_episode_04 idx_episode_05 idx_episode_06 idx_episode_07 idx_episode_08 idx_episode_09 idx_episode_10 idx_episode_11 idx_episode_12 idx_episode_13 idx_episode_14 idx_episode_15 idx_episode_16 idx_episode_17 idx_episode_18 idx_episode_19 idx_episode_20 idx_episode_21 idx_episode_22 idx_episode_23 idx_episode_24 idx_episode_25 idx_episode_26 idx_episode_27 idx_episode_28 idx_episode_29 idx_episode_30 idx_episode_31 idx_episode_32 idx_episode_33 idx_episode_34 idx_episode_35 idx_episode_36 idx_episode_37 idx_episode_38 idx_episode_39 --merge;
