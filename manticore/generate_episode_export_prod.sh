29dea2624554cb0b68b352da1a4abc7782f1dee6
Mitch Downey <mitchldowney@gmail.com>
2022-03-04 23:33:32 -0600

    Update Manticore episode_export handling

--- manticore/generate_episode_export_prod.sh
+++ manticore/generate_episode_export_prod.sh
@@ -1,4 +1,4 @@
-psql -h 0.0.0.0 -p 5432 -U postgres -c "COPY episodes(id, title, "\"pastHourTotalUniquePageviews\"", "\"pastDayTotalUniquePageviews\"", "\"pastWeekTotalUniquePageviews\"", "\"pastMonthTotalUniquePageviews\"", "\"pastYearTotalUniquePageviews\"", "\"pastAllTimeTotalUniquePageviews\"") TO stdout DELIMITER ',' CSV;" > /mnt/podverse_db_prod_volume/manticore/data/episode_export.csv;
-awk '{printf "%s,%s\n", NR,$0}' /mnt/podverse_db_prod_volume/manticore/data/episode_export.csv > /mnt/podverse_db_prod_volume/manticore/data/episode_export_with_serial_column.csv;
-rm /mnt/podverse_db_prod_volume/manticore/data/episode_export.csv;
+PGPASSWORD=mysecretpw psql -h 0.0.0.0 -p 5432 -U postgres -c "COPY episodes(id, title, "\"pastHourTotalUniquePageviews\"", "\"pastDayTotalUniquePageviews\"", "\"pastWeekTotalUniquePageviews\"", "\"pastMonthTotalUniquePageviews\"", "\"pastYearTotalUniquePageviews\"", "\"pastAllTimeTotalUniquePageviews\"") TO stdout DELIMITER ',' CSV;" > /home/mitch/podverse-ops/manticore/episode_export.csv;
+awk '{printf "%s,%s\n", NR,$0}' /home/mitch/podverse-ops/manticore/episode_export.csv > /home/mitch/podverse-ops/manticore/episode_export_with_serial_column.csv;
+rm /home/mitch/podverse-ops/manticore/episode_export.csv;
 docker exec -it podverse_manticore_prod gosu manticore indexer --all --rotate;
