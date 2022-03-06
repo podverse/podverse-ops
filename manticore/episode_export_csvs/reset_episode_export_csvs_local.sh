5b8d308d093ea0f56118c2225a725191ce106c8c
Mitch Downey <mitchldowney@gmail.com>
2022-03-06 00:38:30 -0600

    Create reset_episode_export_csvs_local.sh

---
+++ manticore/episode_export_csvs/reset_episode_export_csvs_local.sh
@@ -0,0 +1,9 @@
+for i in `seq -w 0 9`
+do
+    cp /dev/null /Users/Mitch/Repos/podverse-ops/manticore/episode_export_csvs/episode_export_0$i.csv
+done
+
+for i in `seq -w 10 99`
+do
+    cp /dev/null /Users/Mitch/Repos/podverse-ops/manticore/episode_export_csvs/episode_export_$i.csv
+done
