a627d7e3314d682e85a4361edcc3724d306d4490
Mitch Downey <mitchldowney@gmail.com>
2022-03-07 17:19:30 -0600

    Remove episode_export_csvs files

--- manticore/episode_export_csvs/generate_episode_export_csv_placeholders.sh
+++
@@ -1,9 +0,0 @@
-for i in `seq -w 1 9`
-do
-  touch /Users/Mitch/Repos/podverse-ops/manticore/episode_export_csvs/episode_export_0$i.csv
-done
-
-for i in `seq -w 10 99`
-do
-  touch /Users/Mitch/Repos/podverse-ops/manticore/episode_export_csvs/episode_export_$i.csv
-done
--- manticore/episode_export_csvs/reset_episode_export_csvs_prod.sh
+++
@@ -1,9 +0,0 @@
-for i in `seq -w 0 9`
-do
-    cp /dev/null /home/mitch/podverse-ops/manticore/episode_export_csvs/episode_export_0$i.csv
-done
-
-for i in `seq -w 10 99`
-do
-    cp /dev/null /home/mitch/podverse-ops/manticore/episode_export_csvs/episode_export_$i.csv
-done
