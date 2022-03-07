dcac0bb17fe22b78684dbee4368f7fd7b896feed
Mitch Downey <mitchldowney@gmail.com>
2022-03-06 19:54:19 -0600

    Add --rotate to generate_indexes scripts

--- manticore/generate_indexes_init_local.sh
+++ manticore/generate_indexes_init_local.sh
@@ -6,4 +6,4 @@ docker exec -it podverse_manticore_local gosu manticore indexer idx_author --ver
 docker exec -it podverse_manticore_local gosu manticore indexer idx_media_ref --verbose;
 docker exec -it podverse_manticore_local gosu manticore indexer idx_playlist --verbose;
 docker exec -it podverse_manticore_local gosu manticore indexer idx_podcast --verbose;
-docker exec -it podverse_manticore_local gosu manticore indexer idx_episode_00 --verbose;
+docker exec -it podverse_manticore_local gosu manticore indexer idx_episode_00 --rotate --verbose;
