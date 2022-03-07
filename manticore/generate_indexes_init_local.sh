4e75bc0e712eb806223c1e2b85cd500f76097921
Mitch Downey <mitchldowney@gmail.com>
2022-03-07 11:58:43 -0600

    Remove episode_export split index handling; add create_date column to podcasts, episodes, mediaRefs

--- manticore/generate_indexes_init_local.sh
+++ manticore/generate_indexes_init_local.sh
@@ -1,9 +1,8 @@
-PGPASSWORD='mysecretpw' psql -h 0.0.0.0 -p 5432 -U postgres -c "COPY episodes(id, title, "\"pastHourTotalUniquePageviews\"", "\"pastDayTotalUniquePageviews\"", "\"pastWeekTotalUniquePageviews\"", "\"pastMonthTotalUniquePageviews\"", "\"pastYearTotalUniquePageviews\"", "\"pastAllTimeTotalUniquePageviews\"") TO stdout DELIMITER ',' CSV;" > /Users/Mitch/Repos/podverse-ops/manticore/episode_export.csv;
+PGPASSWORD='mysecretpw' psql -h 0.0.0.0 -p 5432 -U postgres -c "COPY (SELECT id, title, "\"pastHourTotalUniquePageviews\"", "\"pastDayTotalUniquePageviews\"", "\"pastWeekTotalUniquePageviews\"", "\"pastMonthTotalUniquePageviews\"", "\"pastYearTotalUniquePageviews\"", "\"pastAllTimeTotalUniquePageviews\"", extract(epoch from "\"pubDate\"") FROM episodes) TO stdout DELIMITER ',' CSV;" > /Users/Mitch/Repos/podverse-ops/manticore/episode_export.csv;
 awk '{printf "%s,%s\n", NR,$0}' /Users/Mitch/Repos/podverse-ops/manticore/episode_export.csv > /Users/Mitch/Repos/podverse-ops/manticore/episode_export_with_serial_column.csv;
 rm /Users/Mitch/Repos/podverse-ops/manticore/episode_export.csv;
-mv /Users/Mitch/Repos/podverse-ops/manticore/episode_export_with_serial_column.csv /Users/Mitch/Repos/podverse-ops/manticore/episode_export_csvs/episode_export_00.csv;
 docker exec -it podverse_manticore_local gosu manticore indexer idx_author --verbose;
 docker exec -it podverse_manticore_local gosu manticore indexer idx_media_ref --verbose;
 docker exec -it podverse_manticore_local gosu manticore indexer idx_playlist --verbose;
 docker exec -it podverse_manticore_local gosu manticore indexer idx_podcast --verbose;
-docker exec -it podverse_manticore_local gosu manticore indexer idx_episode_00 --rotate --verbose;
+docker exec -it podverse_manticore_local gosu manticore indexer idx_episode --rotate --verbose;
