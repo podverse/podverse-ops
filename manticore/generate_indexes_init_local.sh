13b9cf8861164bf243fd9c150236e415b35d720c
Mitch Downey <mitchldowney@gmail.com>
2022-03-12 13:33:43 -0600

    Remove csv file from episode manticore index job

--- manticore/generate_indexes_init_local.sh
+++ manticore/generate_indexes_init_local.sh
@@ -1,6 +1,3 @@
-PGPASSWORD='mysecretpw' psql -h 0.0.0.0 -p 5432 -U postgres -c "COPY (SELECT id, title, "\"pastHourTotalUniquePageviews\"", "\"pastDayTotalUniquePageviews\"", "\"pastWeekTotalUniquePageviews\"", "\"pastMonthTotalUniquePageviews\"", "\"pastYearTotalUniquePageviews\"", "\"pastAllTimeTotalUniquePageviews\"", extract(epoch from "\"pubDate\"") FROM episodes) TO stdout DELIMITER ',' CSV;" > /Users/Mitch/Repos/podverse-ops/manticore/episode_export.csv;
-awk '{printf "%s,%s\n", NR,$0}' /Users/Mitch/Repos/podverse-ops/manticore/episode_export.csv > /Users/Mitch/Repos/podverse-ops/manticore/episode_export_with_serial_column.csv;
-rm /Users/Mitch/Repos/podverse-ops/manticore/episode_export.csv;
 docker exec -it podverse_manticore_local gosu manticore indexer idx_author --verbose;
 docker exec -it podverse_manticore_local gosu manticore indexer idx_media_ref --verbose;
 docker exec -it podverse_manticore_local gosu manticore indexer idx_playlist --verbose;
