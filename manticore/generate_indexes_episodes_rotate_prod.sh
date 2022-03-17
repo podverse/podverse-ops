98c3daf989476fafc8cb5f8471eed0798d2ce1d0
Mitch Downey <mitchldowney@gmail.com>
2022-03-17 14:08:50 -0500

    Create generate_indexes_episodes_rotate_prod.sh

---
+++ manticore/generate_indexes_episodes_rotate_prod.sh
@@ -0,0 +1,10 @@
+docker exec -it podverse_manticore_prod gosu manticore indexer idx_episode_01 --rotate --verbose;
+docker exec -it podverse_manticore_prod gosu manticore indexer idx_episode_02 --rotate --verbose;
+docker exec -it podverse_manticore_prod gosu manticore indexer idx_episode_03 --rotate --verbose;
+docker exec -it podverse_manticore_prod gosu manticore indexer idx_episode_04 --rotate --verbose;
+docker exec -it podverse_manticore_prod gosu manticore indexer idx_episode_05 --rotate --verbose;
+docker exec -it podverse_manticore_prod gosu manticore indexer idx_episode_06 --rotate --verbose;
+docker exec -it podverse_manticore_prod gosu manticore indexer idx_episode_07 --rotate --verbose;
+docker exec -it podverse_manticore_prod gosu manticore indexer idx_episode_08 --rotate --verbose;
+docker exec -it podverse_manticore_prod gosu manticore indexer idx_episode_09 --rotate --verbose;
+docker exec -it podverse_manticore_prod gosu manticore indexer idx_episode_10 --rotate --verbose;
