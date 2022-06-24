078fbcd1a85fae263018bec2c32a8be81d3d8c36
Mitch Downey <mitchldowney@gmail.com>
2022-06-23 19:38:16 -0500

    Stagger live parser containers

--- scripts/runLatestPodverseApiWorkers.sh
+++ scripts/runLatestPodverseApiWorkers.sh
@@ -9,5 +9,6 @@ docker image rm podverse/podverse_api
 /usr/local/bin/docker-compose -f /home/mitch/podverse-ops/docker-compose.$1.worker.yml run -d --name podverse_api_parser_worker_7 podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 60000 priority
 /usr/local/bin/docker-compose -f /home/mitch/podverse-ops/docker-compose.$1.worker.yml run -d --name podverse_api_parser_worker_8 podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 60000 priority
 /usr/local/bin/docker-compose -f /home/mitch/podverse-ops/docker-compose.$1.worker.yml run -d --name podverse_api_parser_worker_9_live podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 60000 live
-/usr/local/bin/docker-compose -f /home/mitch/podverse-ops/docker-compose.$1.worker.yml run -d --name podverse_api_parser_worker_10_live podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 60000 live
+sleep 15;
+/usr/local/bin/docker-compose -f /home/mitch/podverse-ops/docker-compose.$1.worker.yml run -d --name podverse_api_parser_worker_10_live podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 90000 live
 /usr/local/bin/docker-compose -f /home/mitch/podverse-ops/docker-compose.$1.worker.yml run -d --name runLiveItemListener podverse_api_parser_worker npm run scripts:podping:runLiveItemListener
