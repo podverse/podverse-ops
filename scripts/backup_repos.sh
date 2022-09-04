67e35227bf26ec8debf10e07ece60259faec7ec1
suorcd <suorcd@fastmail.fm>
2022-09-04 15:18:51 -0700

    shellcheck cleanup

--- scripts/backup_repos.sh
+++ scripts/backup_repos.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-cd $1/podverse-api
+[ -z "${1}" ] && exit 1
+
+cd "${1}/podverse-api" || eixt 2
 git pull origin develop
 
-cd $1/podverse-fdroid
+cd "$1/podverse-fdroid" || eixt 2
 git pull origin develop
 
-cd $1/podverse-ops
+cd "$1/podverse-ops" || eixt 2
 git pull origin master
 
-cd $1/podverse-rn
+cd "$1/podverse-rn" || eixt 2
 git pull origin develop
 
-cd $1/podverse-web
+cd "$1/podverse-web" || eixt 2
 git pull origin develop
 
-cd $1/v4v-wallet-browser-extension
+cd "$1/v4v-wallet-browser-extension" || eixt 2
 git pull origin main
