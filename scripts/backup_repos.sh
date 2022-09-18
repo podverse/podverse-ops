130a05fa51b03cd29dd18859a28d85c942b17f27
su || cd <60907848+suorcd@users.noreply.github.com>
2022-09-17 21:29:18 -0700

    fix typo, update code

--- scripts/backup_repos.sh
+++ scripts/backup_repos.sh
@@ -2,20 +2,20 @@
 
 [ -z "${1}" ] && exit 1
 
-cd "${1}/podverse-api" || eixt 2
+cd "${1}/podverse-api" || exit 33
 git pull origin develop
 
-cd "$1/podverse-fdroid" || eixt 2
+cd "$1/podverse-fdroid" || exit 33
 git pull origin develop
 
-cd "$1/podverse-ops" || eixt 2
+cd "$1/podverse-ops" || exit 33
 git pull origin master
 
-cd "$1/podverse-rn" || eixt 2
+cd "$1/podverse-rn" || exit 33
 git pull origin develop
 
-cd "$1/podverse-web" || eixt 2
+cd "$1/podverse-web" || exit 33
 git pull origin develop
 
-cd "$1/v4v-wallet-browser-extension" || eixt 2
+cd "$1/v4v-wallet-browser-extension" || exit 33
 git pull origin main
