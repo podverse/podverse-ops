b3eb262cc27052b846a2c220283ed1c57ea68585
Mitch Downey <mitchldowney@gmail.com>
2022-10-08 18:14:00 -0500

    Update docker-compose files

---
+++ scripts/docker_containers_down_local.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker-compose -f ./docker-compose/local/docker-compose.yml down;
---
+++ scripts/docker_containers_up_local.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+docker-compose -f ./docker-compose/local/docker-compose.yml up podverse_db -d;
+sleep 10;
+rm -rf ./manticore/data;
+docker-compose -f ./docker-compose/local/docker-compose.yml up podverse_manticore -d;
+sleep 5;
+./manticore/generate_indexes_init_local.sh;
+docker restart podverse_manticore_local;
+docker-compose -f ./docker-compose/local/docker-compose.yml up podverse_api -d;
+docker-compose -f ./docker-compose/local/docker-compose.yml up podverse_web -d;
