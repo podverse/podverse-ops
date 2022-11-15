4d95b7c185ebbec4eddd555fff259d5d944ed0b5
Mitch Downey <mitchldowney@gmail.com>
2022-11-14 18:47:51 -0600

    Add 4 more parser workers (12 priority + 2 live = 14 total)

--- scripts/runLatestPodverseApiWorkers.sh
+++ scripts/runLatestPodverseApiWorkers.sh
@@ -10,7 +10,11 @@ docker image rm podverse/podverse_api
 /usr/local/bin/docker-compose -f "/home/mitch/podverse-ops/docker-compose.${1}.worker.yml" run -d --name podverse_api_parser_worker_6 podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 60000 priority
 /usr/local/bin/docker-compose -f "/home/mitch/podverse-ops/docker-compose.${1}.worker.yml" run -d --name podverse_api_parser_worker_7 podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 60000 priority
 /usr/local/bin/docker-compose -f "/home/mitch/podverse-ops/docker-compose.${1}.worker.yml" run -d --name podverse_api_parser_worker_8 podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 60000 priority
-/usr/local/bin/docker-compose -f "/home/mitch/podverse-ops/docker-compose.${1}.worker.yml" run -d --name podverse_api_parser_worker_9_live podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 60000 live
+/usr/local/bin/docker-compose -f "/home/mitch/podverse-ops/docker-compose.${1}.worker.yml" run -d --name podverse_api_parser_worker_9 podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 60000 priority
+/usr/local/bin/docker-compose -f "/home/mitch/podverse-ops/docker-compose.${1}.worker.yml" run -d --name podverse_api_parser_worker_10 podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 60000 priority
+/usr/local/bin/docker-compose -f "/home/mitch/podverse-ops/docker-compose.${1}.worker.yml" run -d --name podverse_api_parser_worker_11 podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 60000 priority
+/usr/local/bin/docker-compose -f "/home/mitch/podverse-ops/docker-compose.${1}.worker.yml" run -d --name podverse_api_parser_worker_12 podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 60000 priority
+/usr/local/bin/docker-compose -f "/home/mitch/podverse-ops/docker-compose.${1}.worker.yml" run -d --name podverse_api_parser_worker_13_live podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 60000 live
 sleep 15;
-/usr/local/bin/docker-compose -f "/home/mitch/podverse-ops/docker-compose.${1}.worker.yml" run -d --name podverse_api_parser_worker_10_live podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 90000 live
+/usr/local/bin/docker-compose -f "/home/mitch/podverse-ops/docker-compose.${1}.worker.yml" run -d --name podverse_api_parser_worker_14_live podverse_api_parser_worker npm run scripts:parseFeedUrlsFromQueue -- 90000 live
 /usr/local/bin/docker-compose -f "/home/mitch/podverse-ops/docker-compose.${1}.worker.yml" run -d --name runLiveItemListener podverse_api_parser_worker npm run scripts:podping:runLiveItemListener
