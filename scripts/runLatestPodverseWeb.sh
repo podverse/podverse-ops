6e0b04d724c399b4043979cb820fae14385b8e70
Mitch Downey <mitchldowney@gmail.com>
2022-11-14 18:52:42 -0600

    Revert stop + remove container command in scripts

--- scripts/runLatestPodverseWeb.sh
+++ scripts/runLatestPodverseWeb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker rm "$(docker stop "$(docker ps -a -q --filter ancestor=podverse/podverse_web)")"
+docker rm $(docker stop $(docker ps -a -q --filter ancestor=podverse/podverse_api))
 docker image rm podverse/podverse_web
 docker-compose -f "/home/mitch/podverse-ops/docker-compose.${1}.yml" up -d podverse_web
