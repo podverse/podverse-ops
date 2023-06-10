5033937959c42b325f1ddffb1c9c9a30b8784ad6
Mitch Downey <mitchldowney@gmail.com>
2023-06-09 22:48:11 -0500

    Update updateSchema.sh

--- db/schema-only/updateSchema.sh
+++ db/schema-only/updateSchema.sh
@@ -1,2 +1,2 @@
-pg_dump -s -h localhost -p 5432 -U postgres -W > ./schema-only.sql
-gzip -c ./schema-only.sql > ./schema-only.sql.gz
+pg_dump -s -h localhost -p 5432 -U postgres -W > ./db/schema-only/schema-only.sql
+gzip -c ./db/schema-only/schema-only.sql > ./db/schema-only/schema-only.sql.gz
