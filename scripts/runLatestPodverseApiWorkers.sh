2a6523ffc95fd8740b0040123515d50a07ddf8a6
Archie Brentano <commit@bobot.lol>
2024-02-19 15:09:21 -0800

    commiting the changes for another api worker called altWorker

--- scripts/runLatestPodverseApiWorkers.sh
+++ scripts/runLatestPodverseApiWorkers.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
-
 docker rm $(docker stop $(docker ps -a -q --filter ancestor=podverse/podverse_api))
 docker image rm podverse/podverse_api
-/usr/local/bin/docker-compose -f "/opt/podverse-ops/docker-compose/${1}/api-worker/docker-compose.yml" run -d --name podverse_api_parser_worker_1 podverse_api_worker npm run scripts:parseFeedUrlsFromQueue -- 60000 priority
-/usr/local/bin/docker-compose -f "/opt/podverse-ops/docker-compose/${1}/api-worker/docker-compose.yml" run -d --name podverse_api_parser_worker_2 podverse_api_worker npm run scripts:parseFeedUrlsFromQueue -- 60000 priority
+#/usr/local/bin/docker-compose -f "/opt/podverse-ops/docker-compose/${1}/api-worker/docker-compose.yml" run -d --name podverse_api_parser_worker_1 podverse_api_worker npm run scripts:parseFeedUrlsFromQueue -- 60000 priority
+#/usr/local/bin/docker-compose -f "/opt/podverse-ops/docker-compose/${1}/api-worker/docker-compose.yml" run -d --name podverse_api_parser_worker_2 podverse_api_worker npm run scripts:parseFeedUrlsFromQueue -- 60000 priority
 /usr/local/bin/docker-compose -f "/opt/podverse-ops/docker-compose/${1}/api-worker/docker-compose.yml" run -d --name podverse_api_parser_worker_3 podverse_api_worker npm run scripts:parseFeedUrlsFromQueue -- 60000 priority
 /usr/local/bin/docker-compose -f "/opt/podverse-ops/docker-compose/${1}/api-worker/docker-compose.yml" run -d --name podverse_api_parser_worker_4 podverse_api_worker npm run scripts:parseFeedUrlsFromQueue -- 60000 priority
 /usr/local/bin/docker-compose -f "/opt/podverse-ops/docker-compose/${1}/api-worker/docker-compose.yml" run -d --name podverse_api_parser_worker_5 podverse_api_worker npm run scripts:parseFeedUrlsFromQueue -- 60000 priority
