0f90c1c85e3a2a5a88829ce59e025448d08d5ecc
Mitch Downey <mitchldowney@gmail.com>
2024-09-09 05:40:58 -0500

    Update npm-link-modules.sh

--- dev/npm-link-modules.sh
+++ dev/npm-link-modules.sh
@@ -12,36 +12,56 @@ echo "Installing podverse-external-services dependencies..."
 cd ../podverse-external-services
 rm -rf node_modules
 npm install
-npm link
+
 
 echo "Installing podverse-helpers dependencies..."
 cd ../podverse-helpers
 rm -rf node_modules
 npm install
-npm link
 
 echo "Installing podverse-orm dependencies..."
 cd ../podverse-orm
 rm -rf node_modules
 npm install
-npm link
 
 echo "Installing podverse-parser dependencies..."
 cd ../podverse-parser
 rm -rf node_modules
 npm install
-npm link
 
 echo "Installing podverse-queue dependencies..."
 cd ../podverse-queue
 rm -rf node_modules
 npm install
-npm link
 
 echo "Installing podverse-workers dependencies..."
 cd ../podverse-workers
 rm -rf node_modules
 npm install
+
+
+
+echo "Linking podverse-external-services dependency..."
+cd ../podverse-external-services
+npm link
+
+echo "Linking podverse-helpers dependency..."
+cd ../podverse-helpers
+npm link
+
+echo "Linking podverse-orm dependency..."
+cd ../podverse-orm
+npm link
+
+echo "Linking podverse-parser dependency..."
+cd ../podverse-parser
+npm link
+
+echo "Linking podverse-queue dependency..."
+cd ../podverse-queue
+npm link
+
+
 
 echo "Linking podverse-api dependencies..."
 cd ../podverse-api
@@ -73,4 +93,5 @@ echo "Linking podverse-workers dependencies..."
 cd ../podverse-workers
 npm link podverse-helpers
 npm link podverse-orm
+npm link podverse-parser
 npm link podverse-queue
