bf2403fec91c8be3f628ef827473a0697f9b74e6
Mitch Downey <mitchldowney@gmail.com>
2024-09-21 16:56:54 -0500

    Fix OneToOne unique relationships; add indexes to remote_item tables

--- database/combined/init_database.sql
+++ database/combined/init_database.sql
@@ -149,7 +149,7 @@ EXECUTE FUNCTION set_updated_at_field();
 
 CREATE TABLE feed_log (
     id SERIAL PRIMARY KEY,
-    feed_id INTEGER NOT NULL REFERENCES feed(id) ON DELETE CASCADE,
+    feed_id INTEGER NOT NULL UNIQUE REFERENCES feed(id) ON DELETE CASCADE,
     last_http_status INTEGER,
     last_good_http_status_time server_time,
     last_finished_parse_time server_time,
@@ -165,7 +165,7 @@ CREATE TABLE channel (
     id SERIAL PRIMARY KEY,
     id_text short_id_v2 UNIQUE NOT NULL,
     slug varchar_slug,
-    feed_id INTEGER NOT NULL REFERENCES feed(id) ON DELETE CASCADE,
+    feed_id INTEGER NOT NULL UNIQUE REFERENCES feed(id) ON DELETE CASCADE,
     podcast_index_id INTEGER UNIQUE NOT NULL,
     podcast_guid UUID UNIQUE, -- <podcast:guid>
     title varchar_normal,
@@ -175,6 +175,10 @@ CREATE TABLE channel (
     -- channels that have a PI value tag require special handling to request value data
     -- from the Podcast Index API.
     has_podcast_index_value BOOLEAN DEFAULT FALSE,
+
+    -- this column is used for optimization purposes to determine if all of the items
+    -- for a channel need to have their value time split remote items parsed.
+    has_value_time_splits BOOLEAN DEFAULT FALSE,
 
     -- hidden items are no longer available in the rss feed, but are still in the database.
     hidden BOOLEAN DEFAULT FALSE,
@@ -202,7 +206,7 @@ INSERT INTO channel_itunes_type (itunes_type) VALUES ('episodic'), ('serial');
 -- various <channel> child data from multiple tags
 CREATE TABLE channel_about (
     id SERIAL PRIMARY KEY,
-    channel_id INTEGER NOT NULL REFERENCES channel(id) ON DELETE CASCADE,
+    channel_id INTEGER NOT NULL UNIQUE REFERENCES channel(id) ON DELETE CASCADE,
     author varchar_normal, -- <itunes:author> and <author>
     episode_count INTEGER, -- aggregated count for convenience
     explicit BOOLEAN, -- <itunes:explicit>
@@ -231,7 +235,7 @@ CREATE INDEX idx_channel_category_parent_id ON channel_category(parent_id);
 -- <channel> -> <podcast:chat>
 CREATE TABLE channel_chat (
     id SERIAL PRIMARY KEY,
-    channel_id INTEGER NOT NULL REFERENCES channel(id) ON DELETE CASCADE,
+    channel_id INTEGER NOT NULL UNIQUE REFERENCES channel(id) ON DELETE CASCADE,
     server varchar_fqdn NOT NULL,
     protocol varchar_short NOT NULL,
     account_id varchar_normal,
@@ -245,8 +249,7 @@ CREATE INDEX idx_channel_chat_channel_id ON channel_chat(channel_id);
 -- <channel> -> <description> AND possibly other tags that contain a description
 CREATE TABLE channel_description (
     id SERIAL PRIMARY KEY,
-    channel_id INTEGER NOT NULL REFERENCES channel(id) ON DELETE CASCADE,
-    UNIQUE (channel_id),
+    channel_id INTEGER NOT NULL UNIQUE REFERENCES channel(id) ON DELETE CASCADE,
     value varchar_long NOT NULL
 );
 
@@ -298,8 +301,7 @@ CREATE INDEX idx_channel_internal_settings_channel_id ON channel_internal_settin
 -- <channel> -> <podcast:license>
 CREATE TABLE channel_license (
     id SERIAL PRIMARY KEY,
-    channel_id INTEGER NOT NULL REFERENCES channel(id) ON DELETE CASCADE,
-    UNIQUE (channel_id),
+    channel_id INTEGER NOT NULL UNIQUE REFERENCES channel(id) ON DELETE CASCADE,
     identifier varchar_normal NOT NULL,
     url varchar_url
 );
@@ -311,7 +313,7 @@ CREATE INDEX idx_channel_license_channel_id ON channel_license(channel_id);
 -- <channel> -> <podcast:location>
 CREATE TABLE channel_location (
     id SERIAL PRIMARY KEY,
-    channel_id INTEGER NOT NULL REFERENCES channel(id) ON DELETE CASCADE,
+    channel_id INTEGER NOT NULL UNIQUE REFERENCES channel(id) ON DELETE CASCADE,
     geo varchar_normal,
     osm varchar_normal,
     CHECK (geo IS NOT NULL OR osm IS NOT NULL),
@@ -340,7 +342,7 @@ CREATE INDEX idx_channel_person_channel_id ON channel_person(channel_id);
 -- <channel> -> <podcast:podroll>
 CREATE TABLE channel_podroll (
     id SERIAL PRIMARY KEY,
-    channel_id INTEGER NOT NULL REFERENCES channel(id) ON DELETE CASCADE
+    channel_id INTEGER NOT NULL UNIQUE REFERENCES channel(id) ON DELETE CASCADE
 );
 
 CREATE INDEX idx_channel_podroll_channel_id ON channel_podroll(channel_id);
@@ -360,13 +362,16 @@ CREATE TABLE channel_podroll_remote_item (
 
 CREATE INDEX idx_channel_podroll_remote_item_channel_podroll_id ON channel_podroll_remote_item(channel_podroll_id);
 CREATE INDEX idx_channel_podroll_remote_item_medium_id ON channel_podroll_remote_item(medium_id);
+CREATE INDEX idx_channel_podroll_remote_item_feed_guid ON channel_podroll_remote_item(feed_guid);
+CREATE INDEX idx_channel_podroll_remote_item_feed_url ON channel_podroll_remote_item(feed_url);
+CREATE INDEX idx_channel_podroll_remote_item_item_guid ON channel_podroll_remote_item(item_guid);
 
 --** CHANNEL > PUBLISHER
 
 -- <channel> -> <podcast:publisher>
 CREATE TABLE channel_publisher (
     id SERIAL PRIMARY KEY,
-    channel_id INTEGER NOT NULL REFERENCES channel(id) ON DELETE CASCADE
+    channel_id INTEGER NOT NULL UNIQUE REFERENCES channel(id) ON DELETE CASCADE
 );
 
 CREATE INDEX idx_channel_publisher_channel_id ON channel_publisher(channel_id);
@@ -376,7 +381,7 @@ CREATE INDEX idx_channel_publisher_channel_id ON channel_publisher(channel_id);
 -- <channel> -> <podcast:publisher> -> <podcast:remoteItem>
 CREATE TABLE channel_publisher_remote_item (
     id SERIAL PRIMARY KEY,
-    channel_publisher_id INTEGER NOT NULL REFERENCES channel_publisher(id) ON DELETE CASCADE,
+    channel_publisher_id INTEGER NOT NULL UNIQUE REFERENCES channel_publisher(id) ON DELETE CASCADE,
     feed_guid UUID NOT NULL,
     feed_url varchar_url,
     item_guid varchar_uri,
@@ -386,6 +391,9 @@ CREATE TABLE channel_publisher_remote_item (
 
 CREATE INDEX idx_channel_publisher_remote_item_channel_publisher_id ON channel_publisher_remote_item(channel_publisher_id);
 CREATE INDEX idx_channel_publisher_remote_item_medium_id ON channel_publisher_remote_item(medium_id);
+CREATE INDEX idx_channel_publisher_remote_item_feed_guid ON channel_publisher_remote_item(feed_guid);
+CREATE INDEX idx_channel_publisher_remote_item_feed_url ON channel_publisher_remote_item(feed_url);
+CREATE INDEX idx_channel_publisher_remote_item_item_guid ON channel_publisher_remote_item(item_guid);
 
 --** CHANNEL > REMOTE ITEM
 
@@ -405,6 +413,9 @@ CREATE TABLE channel_remote_item (
 
 CREATE INDEX idx_channel_remote_item_channel_id ON channel_remote_item(channel_id);
 CREATE INDEX idx_channel_remote_item_medium_id ON channel_remote_item(medium_id);
+CREATE INDEX idx_channel_remote_item_feed_guid ON channel_remote_item(feed_guid);
+CREATE INDEX idx_channel_remote_item_feed_url ON channel_remote_item(feed_url);
+CREATE INDEX idx_channel_remote_item_item_guid ON channel_remote_item(item_guid);
 
 --** CHANNEL > SEASON
 
@@ -539,7 +550,7 @@ INSERT INTO item_itunes_episode_type (itunes_episode_type) VALUES ('full'), ('tr
 
 CREATE TABLE item_about (
     id SERIAL PRIMARY KEY,
-    item_id INTEGER NOT NULL REFERENCES item(id) ON DELETE CASCADE,
+    item_id INTEGER NOT NULL UNIQUE REFERENCES item(id) ON DELETE CASCADE,
     duration media_player_time, -- <itunes:duration>
     explicit BOOLEAN, -- <itunes:explicit>
     website_link_url varchar_url, -- <link>
@@ -554,7 +565,7 @@ CREATE INDEX idx_item_about_item_itunes_episode_type_id ON item_about(item_itune
 -- <item> -> <podcast:chapters>
 CREATE TABLE item_chapters_feed (
     id SERIAL PRIMARY KEY,
-    item_id INTEGER NOT NULL REFERENCES item(id) ON DELETE CASCADE,
+    item_id INTEGER NOT NULL UNIQUE REFERENCES item(id) ON DELETE CASCADE,
     url varchar_url NOT NULL,
     type varchar_short NOT NULL
 );
@@ -567,7 +578,7 @@ CREATE INDEX idx_item_chapters_feed_item_id ON item_chapters_feed(item_id);
 
 CREATE TABLE item_chapters_feed_log (
     id SERIAL PRIMARY KEY,
-    item_chapters_feed_id INTEGER NOT NULL REFERENCES item_chapters_feed(id) ON DELETE CASCADE,
+    item_chapters_feed_id INTEGER NOT NULL UNIQUE REFERENCES item_chapters_feed(id) ON DELETE CASCADE,
     last_http_status INTEGER,
     last_good_http_status_time server_time,
     last_finished_parse_time server_time,
@@ -598,7 +609,7 @@ CREATE INDEX idx_item_chapter_item_chapters_feed_id ON item_chapter(item_chapter
 -- <item> -> <podcast:chapters> -> chapter items correspond with jsonChapters.md example file
 CREATE TABLE item_chapter_location (
     id SERIAL PRIMARY KEY,
-    item_chapter_id INTEGER NOT NULL REFERENCES item_chapter(id) ON DELETE CASCADE,
+    item_chapter_id INTEGER NOT NULL UNIQUE REFERENCES item_chapter(id) ON DELETE CASCADE,
     geo varchar_normal,
     osm varchar_normal,
     CHECK (geo IS NOT NULL OR osm IS NOT NULL),
@@ -612,7 +623,7 @@ CREATE INDEX idx_item_chapter_location_item_chapter_id ON item_chapter_location(
 -- <item> -> <podcast:chat>
 CREATE TABLE item_chat (
     id SERIAL PRIMARY KEY,
-    item_id INTEGER NOT NULL REFERENCES item(id) ON DELETE CASCADE,
+    item_id INTEGER NOT NULL UNIQUE REFERENCES item(id) ON DELETE CASCADE,
     server varchar_fqdn NOT NULL,
     protocol varchar_short NOT NULL,
     account_id varchar_normal,
@@ -638,8 +649,7 @@ CREATE INDEX idx_item_content_link_item_id ON item_content_link(item_id);
 -- <item> -> <description> AND possibly other tags that contain a description
 CREATE TABLE item_description (
     id SERIAL PRIMARY KEY,
-    item_id INTEGER NOT NULL REFERENCES item(id) ON DELETE CASCADE,
-    UNIQUE (item_id),
+    item_id INTEGER NOT NULL UNIQUE REFERENCES item(id) ON DELETE CASCADE,
     value varchar_long NOT NULL
 );
 
@@ -679,7 +689,7 @@ CREATE INDEX idx_item_enclosure_source_item_id ON item_enclosure_source(item_enc
 -- <item> -> <podcast:alternateEnclosure> -> <podcast:integrity>
 CREATE TABLE item_enclosure_integrity (
     id SERIAL PRIMARY KEY,
-    item_enclosure_id INTEGER NOT NULL REFERENCES item_enclosure_source(id) ON DELETE CASCADE,
+    item_enclosure_id INTEGER NOT NULL UNIQUE REFERENCES item_enclosure_source(id) ON DELETE CASCADE,
     type TEXT NOT NULL CHECK (type IN ('sri', 'pgp-signature')),
     value varchar_long NOT NULL
 );
@@ -720,8 +730,7 @@ CREATE INDEX idx_item_image_item_id ON item_image(item_id);
 -- <item> -> <podcast:license>
 CREATE TABLE item_license (
     id SERIAL PRIMARY KEY,
-    item_id INTEGER NOT NULL REFERENCES item(id) ON DELETE CASCADE,
-    UNIQUE (item_id),
+    item_id INTEGER NOT NULL UNIQUE REFERENCES item(id) ON DELETE CASCADE,
     identifier varchar_normal NOT NULL,
     url varchar_url
 );
@@ -733,7 +742,7 @@ CREATE INDEX idx_item_license_item_id ON item_license(item_id);
 -- <item> -> <podcast:location>
 CREATE TABLE item_location (
     id SERIAL PRIMARY KEY,
-    item_id INTEGER NOT NULL REFERENCES item(id) ON DELETE CASCADE,
+    item_id INTEGER NOT NULL UNIQUE REFERENCES item(id) ON DELETE CASCADE,
     geo varchar_normal,
     osm varchar_normal,
     CHECK (geo IS NOT NULL OR osm IS NOT NULL),
@@ -775,7 +784,7 @@ CREATE INDEX idx_item_season_item_id ON item_season(item_id);
 -- <item> -> <podcast:season> -> <podcast:episode>
 CREATE TABLE item_season_episode (
     id SERIAL PRIMARY KEY,
-    item_id INTEGER NOT NULL REFERENCES item(id) ON DELETE CASCADE,
+    item_id INTEGER NOT NULL UNIQUE REFERENCES item(id) ON DELETE CASCADE,
     display varchar_short,
     number FLOAT NOT NULL
 );
@@ -886,7 +895,7 @@ CREATE INDEX idx_item_value_time_split_item_value_id ON item_value_time_split(it
 -- <item> -> <podcast:value> -> <podcast:valueTimeSplit> -> <podcast:remoteItem>
 CREATE TABLE item_value_time_split_remote_item (
     id SERIAL PRIMARY KEY,
-    item_value_time_split_id INTEGER NOT NULL REFERENCES item_value_time_split(id) ON DELETE CASCADE,
+    item_value_time_split_id INTEGER NOT NULL UNIQUE REFERENCES item_value_time_split(id) ON DELETE CASCADE,
     feed_guid UUID NOT NULL,
     feed_url varchar_url,
     item_guid varchar_uri,
@@ -894,6 +903,9 @@ CREATE TABLE item_value_time_split_remote_item (
 );
 
 CREATE INDEX idx_item_value_time_split_remote_item_item_value_time_split_id ON item_value_time_split_remote_item(item_value_time_split_id);
+CREATE INDEX idx_item_value_time_split_remote_item_feed_guid ON item_value_time_split_remote_item(feed_guid);
+CREATE INDEX idx_item_value_time_split_remote_item_feed_url ON item_value_time_split_remote_item(feed_url);
+CREATE INDEX idx_item_value_time_split_remote_item_item_guid ON item_value_time_split_remote_item(item_guid);
 
 --** ITEM > VALUE > TIME SPLIT > VALUE RECIPEINT
 
@@ -929,7 +941,7 @@ INSERT INTO live_item_status (status) VALUES ('pending'), ('live'), ('ended');
 -- <channel> -> <podcast:liveItem>
 CREATE TABLE live_item (
     id SERIAL PRIMARY KEY,
-    item_id INTEGER NOT NULL REFERENCES item(id) ON DELETE CASCADE,
+    item_id INTEGER NOT NULL UNIQUE REFERENCES item(id) ON DELETE CASCADE,
     live_item_status_id INTEGER NOT NULL REFERENCES live_item_status(id),
     start_time TIMESTAMPTZ NOT NULL,
     end_time TIMESTAMPTZ,
