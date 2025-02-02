54b1e1e0e10ae83dc882becc77cb4be15e6ab758
Mitch Downey <mitchldowney@gmail.com>
2025-02-01 22:36:37 -0600

    Add not null to category mapping_key

--- database/combined/init_database.sql
+++ database/combined/init_database.sql
@@ -96,7 +96,7 @@ CREATE TABLE category (
     parent_id INTEGER REFERENCES category(id) ON DELETE CASCADE,
     display_name varchar_normal NOT NULL, -- our own display name for the category
     slug varchar_normal NOT NULL, -- our own web url slug for the category
-    mapping_key varchar_normal -- camel case version of the slug
+    mapping_key varchar_normal NOT NULL -- camel case version of the slug
 );
 
 CREATE INDEX idx_category_parent_id ON category(parent_id);
