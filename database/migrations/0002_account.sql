1de1f7a58c38ac5c895880997203937ec4a9467c
Mitch Downey <mitchldowney@gmail.com>
2025-02-13 18:14:54 -0600

    Add unique constraint to account_ tables

--- database/migrations/0002_account.sql
+++ database/migrations/0002_account.sql
@@ -1,5 +1,3 @@
--- 0002 migration
-
 CREATE TABLE sharable_status (
     id SERIAL PRIMARY KEY,
     status TEXT UNIQUE CHECK (status IN ('public', 'unlisted', 'private'))
@@ -18,7 +16,7 @@ CREATE INDEX idx_account_sharable_status_id ON account(sharable_status_id);
 
 CREATE TABLE account_credentials (
     id SERIAL PRIMARY KEY,
-    account_id integer REFERENCES account(id) ON DELETE CASCADE,
+    account_id integer REFERENCES account(id) ON DELETE CASCADE UNIQUE,
     email varchar_email UNIQUE NOT NULL,
     password varchar_password NOT NULL
 );
@@ -27,7 +25,7 @@ CREATE INDEX idx_account_credentials_account_id ON account_credentials(account_i
 
 CREATE TABLE account_profile (
     id SERIAL PRIMARY KEY,
-    account_id integer REFERENCES account(id) ON DELETE CASCADE,
+    account_id integer REFERENCES account(id) ON DELETE CASCADE UNIQUE,
     display_name varchar_normal,
     bio varchar_long
 );
@@ -36,7 +34,7 @@ CREATE INDEX idx_account_profile_account_id ON account_profile(account_id);
 
 CREATE TABLE account_reset_password (
     id SERIAL PRIMARY KEY,
-    account_id integer REFERENCES account(id) ON DELETE CASCADE,
+    account_id integer REFERENCES account(id) ON DELETE CASCADE UNIQUE,
     reset_token varchar_guid,
     reset_token_expires_at TIMESTAMP
 );
@@ -45,7 +43,7 @@ CREATE INDEX idx_account_reset_password_account_id ON account_reset_password(acc
 
 CREATE TABLE account_verification (
     id SERIAL PRIMARY KEY,
-    account_id integer REFERENCES account(id) ON DELETE CASCADE,
+    account_id integer REFERENCES account(id) ON DELETE CASCADE UNIQUE,
     verification_token varchar_guid,
     verification_token_expires_at TIMESTAMP
 );
@@ -61,7 +59,7 @@ INSERT INTO account_membership (tier) VALUES ('trial'), ('basic');
 
 CREATE TABLE account_membership_status (
     id SERIAL PRIMARY KEY,
-    account_id integer REFERENCES account(id) ON DELETE CASCADE,
+    account_id integer REFERENCES account(id) ON DELETE CASCADE UNIQUE,
     account_membership_id INTEGER NOT NULL REFERENCES account_membership(id),
     membership_expires_at TIMESTAMP
 );
@@ -71,7 +69,7 @@ CREATE INDEX idx_account_membership_status_account_membership_id ON account_memb
 
 CREATE TABLE account_admin_roles (
     id SERIAL PRIMARY KEY,
-    account_id integer REFERENCES account(id) ON DELETE CASCADE,
+    account_id integer REFERENCES account(id) ON DELETE CASCADE UNIQUE,
     dev_admin BOOLEAN DEFAULT FALSE,
     podping_admin BOOLEAN DEFAULT FALSE
 );
