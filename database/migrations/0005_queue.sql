fe6cf7c15398dc34e2807ab53bcce50c5a956645
Mitch Downey <mitchldowney@gmail.com>
2025-02-23 11:37:48 -0600

    Adjust queue_resource_base

--- database/migrations/0005_queue.sql
+++ database/migrations/0005_queue.sql
@@ -14,34 +14,34 @@ CREATE INDEX idx_queue_medium_id ON queue(medium_id);
 CREATE TABLE queue_resource (
     id SERIAL PRIMARY KEY,
     queue_id INTEGER NOT NULL REFERENCES queue(id) ON DELETE CASCADE,
-    list_position list_position NOT NULL CHECK (list_position != 0 OR list_position = 0::numeric),
+    list_position list_position NOT NULL,
     playback_position media_player_time NOT NULL DEFAULT 0,
     media_file_duration media_player_time NOT NULL DEFAULT 0,
     completed BOOLEAN NOT NULL DEFAULT FALSE,
     item_id INTEGER REFERENCES item(id) ON DELETE CASCADE,
-    add_by_rss_resource_data jsonb,
-    add_by_rss_hash_id varchar_md5,
     item_chapter_id INTEGER REFERENCES item_chapter(id) ON DELETE CASCADE,
     clip_id INTEGER REFERENCES clip(id) ON DELETE CASCADE,
     item_soundbite_id INTEGER REFERENCES item_soundbite(id) ON DELETE CASCADE,
+    add_by_rss_resource_data jsonb,
+    add_by_rss_hash_id varchar_md5,
     UNIQUE (queue_id, list_position),
     CHECK (
         (item_id IS NOT NULL)::int +
-        (add_by_rss_hash_id IS NOT NULL)::int +
         (item_chapter_id IS NOT NULL)::int +
         (clip_id IS NOT NULL)::int +
         (item_soundbite_id IS NOT NULL)::int = 1
+        (add_by_rss_hash_id IS NOT NULL)::int +
     ),
     UNIQUE (queue_id, item_id),
-    UNIQUE (queue_id, add_by_rss_hash_id),
     UNIQUE (queue_id, item_chapter_id),
     UNIQUE (queue_id, clip_id),
-    UNIQUE (queue_id, item_soundbite_id)
+    UNIQUE (queue_id, item_soundbite_id),
+    UNIQUE (queue_id, add_by_rss_hash_id)
 );
 
 CREATE INDEX idx_queue_resource_queue_id ON queue_resource(queue_id);
 CREATE INDEX idx_queue_resource_item_id ON queue_resource(item_id);
-CREATE INDEX idx_queue_resource_add_by_rss_hash_id ON queue_resource(add_by_rss_hash_id);
 CREATE INDEX idx_queue_resource_item_chapter_id ON queue_resource(item_chapter_id);
 CREATE INDEX idx_queue_resource_clip_id ON queue_resource(clip_id);
 CREATE INDEX idx_queue_resource_soundbite_id ON queue_resource(item_soundbite_id);
+CREATE INDEX idx_queue_resource_add_by_rss_hash_id ON queue_resource(add_by_rss_hash_id);
