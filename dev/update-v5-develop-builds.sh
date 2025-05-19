c56e2df0f60a1a259296ff4579a11c7ae203eb0f
Kyle Downey <capntechbeard@gmail.com>
2025-05-19 15:03:41 -0500

    Add script to update and build all v5-develop repositories

---
+++ dev/update-v5-develop-builds.sh
@@ -0,0 +1,47 @@
+cd ~/repos/podverse-api
+git pull origin v5-develop
+
+cd ~/repos/podverse-external-services
+git pull origin v5-develop
+rm -rf node_modules
+rm -rf dist
+npm install
+npm run build
+
+cd ~/repos/podverse-helpers
+git pull origin v5-develop
+rm -rf node_modules
+rm -rf dist
+npm install
+npm run build
+
+cd ~/repos/podverse-ops
+git pull origin v5-develop
+
+cd ~/repos/podverse-orm
+git pull origin v5-develop
+rm -rf node_modules
+rm -rf dist
+npm install
+npm run build
+
+cd ~/repos/podverse-parser
+git pull origin v5-develop
+rm -rf node_modules
+rm -rf dist
+npm install
+npm run build
+
+cd ~/repos/podverse-queue
+git pull origin v5-develop
+rm -rf node_modules
+rm -rf dist
+npm install
+npm run build
+
+cd ~/repos/podverse-workers
+git pull origin v5-develop
+rm -rf node_modules
+rm -rf dist
+npm install
+npm run build
