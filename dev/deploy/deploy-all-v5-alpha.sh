1fe7ea924c5a5ec52b4e5210e1061b25a4cd14df
Mitch Downey <mitchldowney@gmail.com>
2025-05-27 01:04:19 -0500

    Add deploy script for v5-alpha repositories

---
+++ dev/deploy/deploy-all-v5-alpha.sh
@@ -0,0 +1,67 @@
+cd ~/repos/podverse-helpers
+git checkout v5-develop
+git pull origin v5-develop
+git checkout v5-alpha
+git pull origin v5-develop
+git push origin v5-alpha
+git checkout v5-develop
+
+sleep 60
+
+cd ~/repos/podverse-external-services
+git checkout v5-develop
+git pull origin v5-develop
+git checkout v5-alpha
+git pull origin v5-develop
+git push origin v5-alpha
+git checkout v5-develop
+
+sleep 60
+
+cd ~/repos/podverse-orm
+git checkout v5-develop
+git pull origin v5-develop
+git checkout v5-alpha
+git pull origin v5-develop
+git push origin v5-alpha
+git checkout v5-develop
+
+sleep 60
+
+cd ~/repos/podverse-parser
+git checkout v5-develop
+git pull origin v5-develop
+git checkout v5-alpha
+git pull origin v5-develop
+git push origin v5-alpha
+git checkout v5-develop
+
+sleep 60
+
+cd ~/repos/podverse-queue
+git checkout v5-develop
+git pull origin v5-develop
+git checkout v5-alpha
+git pull origin v5-develop
+git push origin v5-alpha
+git checkout v5-develop
+
+sleep 60
+
+cd ~/repos/podverse-workers
+git checkout v5-develop
+git pull origin v5-develop
+git checkout v5-alpha
+git pull origin v5-develop
+git push origin v5-alpha
+git checkout v5-develop
+
+sleep 60
+
+cd ~/repos/podverse-api
+git checkout v5-develop
+git pull origin v5-develop
+git checkout v5-alpha
+git pull origin v5-develop
+git push origin v5-alpha
+git checkout v5-develop
