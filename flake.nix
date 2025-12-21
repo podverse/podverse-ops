c9aaec482eaee3933d2b2c060eb0cb523a8687b3
archie <suorcd@fastmail.fm>
2025-12-21 09:10:50 -0800

    Update: Add .envrc file and modify flake.nix for Podverse environment

--- flake.nix
+++ flake.nix
@@ -1,10 +1,10 @@
 {
-  description = "Homelab Kubernetes Management Environment";
+  description = "Podverse Kubernetes Management Environment";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    sops-nix.url = "git+https://gitea.ser.ink/Tracking/sops-nix";
+    sops-nix.url = "github:Mic92/sops-nix";
   };
 
   outputs = { self, nixpkgs, flake-utils, sops-nix }:
@@ -15,26 +15,21 @@
       {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            # The Essentials
-            kompose       # The converter
-            kubectl       # The controller
-            kubernetes-helm # If you move to charts later
-            
-            # The "Nice to Haves"
-            k9s           # TUI for managing the cluster (Great in Neovim terminals)
-            yamllint      # Linter for your YAML files
-
-            # Secrets management
-            sops
-            moreutils
-            jq
-            git
             argocd
+            git
+            jq
+            k9s
+            kompose
+            kubectl
+            kubernetes-helm
+            moreutils
+            sops
+            yamllint
+            yq
           ];
 
           shellHook = ''
-            echo "🛠️  Homelab K3s Environment Loaded"
-            echo "run 'k9s' to manage cluster or 'kompose convert' to migrate apps."
+            echo "🛠️  Podverse Kubernetes Management Environment"
             echo "Ensure your KUBECONFIG is set (usually ~/.kube/config or /etc/rancher/k3s/k3s.yaml)"
           '';
         };
