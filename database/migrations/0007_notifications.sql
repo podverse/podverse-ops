d632801a1e152645e6114c5b6ad09b371a374706
Mitch Downey <mitchldowney@gmail.com>
2026-01-08 01:59:23 -0600

    feat(local-utils): Enhance Makefile with new database management commands and update database schema for account_up_device

--- database/migrations/0007_notifications.sql
+++ database/migrations/0007_notifications.sql
@@ -22,9 +22,16 @@ CREATE TABLE account_up_device (
     id SERIAL PRIMARY KEY,
     account_id INTEGER NOT NULL REFERENCES account(id) ON DELETE CASCADE,
     up_endpoint varchar_url UNIQUE NOT NULL,
-    up_public_key varchar_long NOT NULL,
-    up_auth_key varchar_long NOT NULL
+    up_auth_key varchar_long,
+    locale varchar_locale NOT NULL,
+    created_at server_time_with_default NOT NULL,
+    updated_at server_time_with_default NOT NULL
 );
+
+CREATE TRIGGER set_updated_at_account_up_device
+BEFORE UPDATE ON account_up_device
+FOR EACH ROW
+EXECUTE FUNCTION set_updated_at_field();
 
 CREATE INDEX idx_account_up_device_account_id ON account_up_device(account_id);
 CREATE INDEX idx_account_up_device_up_endpoint ON account_up_device(up_endpoint);
