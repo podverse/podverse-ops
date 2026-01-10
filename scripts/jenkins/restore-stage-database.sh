99a59864437cfc258eb2c16ed735a7ad554ff4e3
Mitch Downey <mitchldowney@gmail.com>
2026-01-10 01:29:50 -0600

    delete(restore-stage-database): Remove obsolete script for restoring the stage database

--- scripts/jenkins/restore-stage-database.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker stop podverse_db_stage;
-docker rm podverse_db_stage;
-docker-compose -f ./podverse-ops/docker-compose.stage.yml up -d podverse_db;
-sleep 10;
-PGPASSWORD='<password here>' psql -h localhost -U postgres -d postgres -f old-sample-database.sql;
